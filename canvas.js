a411022ddc576bf32bf03c8f8dea30abd49841b4
Javier Villca <javieranibalvillca@gmail.com>
2022-09-18 15:01:31 -0300

    Agregado mis archivos

---
+++ canvas.js
@@ -0,0 +1,158 @@
+function DibujarBaseAhorcado(){
+    tablero.lineWidth = 8; //ancho de la linea
+    tablero.lineCap = "round"; //los extremos de la linea son redondeados
+    tablero.lineJoin = "round"; //las uniones con otra linea se hacen redondeadas 
+    tablero.fillStyle = "rgba(180,155,125,0)"; //color del relleno del talbero
+    tablero.strokeStyle = "#DB146C"; //color de la linea
+    
+    tablero.fillRect(0,0,1200,860); //define donde usar el area del relleno blanco que elegimos antes
+    tablero.beginPath(); //le indica que moveremos a un lugar el pincel
+    tablero.moveTo(500, 500); //las coordenadas donde apoyamos y comenzamos el pincel
+    tablero.lineTo(650, 500); //coordenadas donde queremos que vaya el pincel
+    tablero.stroke(); //dibuja la linea que definimos con las coordenadas anteriores
+    tablero.closePath(); //une las puntas que se hicieron con moveTo lineTo, ejemplo cerraria L y la convertiria en rectangulo
+}
+
+function dibujarGuiones(){
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round"; 
+    tablero.fillStyle = "#F3F5F6";  
+    tablero.strokeStyle = "#DB146C";
+
+    let anchura = 700/palabraSecreta.length;
+
+    for (let i = 0; i < palabraSecreta.length; i++){
+        tablero.beginPath();
+        tablero.moveTo(300 + (anchura * i), 640);
+        tablero.lineTo(350 + (anchura * i), 640);
+        tablero.stroke();
+        tablero.closePath();
+    }
+}
+
+function dibujarTroncoPrincipal(){
+    tablero.lineWidth = 8;
+    tablero.lineCap = "round"; 
+    tablero.lineJoin = "round"; 
+    tablero.strokeStyle = "#DB146C"; 
+
+    tablero.beginPath();
+    tablero.moveTo(525, 500);
+    tablero.lineTo(525, 200);
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function dibujarTronco2(){
+    tablero.lineWidth = 8;
+    tablero.lineCap = "round"; 
+    tablero.lineJoin = "round"; 
+    tablero.strokeStyle = "#DB146C"; 
+
+    tablero.beginPath();
+    tablero.moveTo(525, 200);
+    tablero.lineTo(600, 200);
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function dibujarTronco3(){
+    tablero.lineWidth = 8;
+    tablero.lineCap = "round"; 
+    tablero.lineJoin = "round"; 
+    tablero.strokeStyle = "#DB146C"; 
+
+    tablero.beginPath();
+    tablero.moveTo(600, 200);
+    tablero.lineTo(600, 250);
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function dibujarCabeza(){
+    tablero.lineWidth = 5;
+    tablero.strokeStyle = "#DB146C"; 
+    tablero.beginPath();
+    tablero.arc(600, 285, 30, 0, 2 * Math.PI);
+    tablero.stroke();
+}
+
+function dibujaTorso(){
+    tablero.lineWidth = 5;
+    tablero.lineCap = "round";
+    tablero.strokeStyle = "#DB146C";
+
+    tablero.beginPath();
+    tablero.moveTo(600, 315);
+    tablero.lineTo(600, 400);
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function dibujarBrazoIz(){
+    tablero.lineWidth = 5;
+    tablero.lineCap = "round";
+    tablero.strokeStyle = "#DB146C";
+
+    tablero.beginPath();
+    tablero.moveTo(600, 335);
+    tablero.lineTo(580, 395);
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function dibujarBrazoDerech(){
+    tablero.lineWidth = 5;
+    tablero.lineCap = "round";
+    tablero.strokeStyle = "#DB146C";
+
+    tablero.beginPath();
+    tablero.moveTo(600, 335);
+    tablero.lineTo(620, 395);
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function dibujarPiernaIz(){
+    tablero.lineWidth = 5;
+    tablero.lineCap = "round";
+    tablero.strokeStyle = "#DB146C";
+
+    tablero.beginPath();
+    tablero.moveTo(600, 400);
+    tablero.lineTo(580, 460);
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function dibujarPiernaDerch(){
+    tablero.lineWidth = 5;
+    tablero.lineCap = "round";
+    tablero.strokeStyle = "#DB146C";
+
+    tablero.beginPath();
+    tablero.moveTo(600, 400);
+    tablero.lineTo(620, 460);
+    tablero.stroke();
+    tablero.closePath();
+}
+
+
+function mensajeVictoria(){
+    tablero.font = "bold 45px 'Montserrat', sans-serif";
+    tablero.fillStyle = "green";
+    tablero.textAlign = "center";
+
+    tablero.fillText("Felicidades", 600, 350);
+    tablero.fillText("Adivinaste la palabra!", 600, 400);
+}
+
+function mensajeDerrota(){
+    tablero.font = "bold 35px 'Montserrat', sans-serif";
+    tablero.fillStyle = "red";
+    tablero.textAlign = "center";
+
+    tablero.fillText("Fin de juego, lo siento perdiste!", 600, 350);
+    tablero.fillText(`La palabra secreta era ${palabraSecreta}`, 600, 400);
+}
---
+++ javascript.js
@@ -0,0 +1,142 @@
+let palabras = ["autos", "libros","computadora", "alura","html", "one", "css", "oracle"];
+let tablero = document.querySelector("#horca").getContext("2d");
+let palabraSecreta = "";
+let palabraSecretaSeparada = "";
+let letraEscrita = "";
+let letrasAcertadas = [];
+let intentosFallidos = 0;
+
+
+document.querySelector("#iniciar-juego").onclick = function(){
+    iniciarJuego();
+    escogerPalabraSecreta();
+    DibujarBaseAhorcado();
+    dibujarGuiones("#DB146C");
+    verBoton();
+    console.log(palabraSecretaSeparada);
+    console.log(palabras);
+}
+
+document.querySelector("#boton-agregar-palabra").onclick = function(){
+    agregarPalabra()
+}
+
+document.querySelector("#letra-boton").onclick = function(){
+    letraEscrita = document.querySelector("#letra-elegida").value.toUpperCase();
+    dibujarLetras();
+    dibujarLetrasUsadas();
+    console.log(letraEscrita);
+}
+
+document.querySelector("#boton-jugar-nuevamente").onclick = function(){
+    location.reload();
+}
+
+function escogerPalabraSecreta(){
+    let palabra = palabras[Math.floor(Math.random() * palabras.length)].toUpperCase();
+    palabraSecreta = palabra;
+    palabraSecretaSeparada = palabra.split("");
+}
+
+function iniciarJuego(){
+    document.querySelector("#frente-pagina").style.display = "none";
+}
+
+function dibujarLetras(){
+    tablero.font = "30px Arial";
+    tablero.fillStyle = "black";
+    tablero.textAlign = "center";
+
+    let anchura = 700/palabraSecretaSeparada.length;
+
+    for (let i = 0; i <palabraSecretaSeparada.length; i++){
+        if (letraEscrita == palabraSecretaSeparada[i] ){
+            tablero.fillText(letraEscrita, 325 + (anchura * i), 630);
+            letrasAcertadas.push(letraEscrita);
+            console.log(letrasAcertadas);
+
+            if(letrasAcertadas.length == palabraSecretaSeparada.length){
+                mensajeVictoria();
+                dibujarGuiones();
+                ocultarBotones();
+                document.querySelector("#boton-jugar-nuevamente").style.display="block";
+            }  
+        }
+        document.querySelector("#letra-elegida").focus();
+        document.querySelector("#letra-elegida").value = "";
+    }
+}
+
+function dibujarLetrasUsadas(){
+
+    
+
+    if(!palabraSecretaSeparada.includes(letraEscrita) && (!letraEscrita == "")){
+
+        letrasErradas();
+        dibujarTroncoPrincipal();
+ 
+            if(intentosFallidos == 2 ){
+                dibujarTronco2();
+            }
+
+            if(intentosFallidos == 3){
+                dibujarTronco3();
+            }
+            
+            if(intentosFallidos == 4){
+                dibujarCabeza();
+            }
+
+            if(intentosFallidos == 5){
+                dibujaTorso();
+            }
+
+            if(intentosFallidos == 6){
+                dibujarBrazoIz()
+            }
+
+            if(intentosFallidos == 7){
+                dibujarBrazoDerech()
+            }
+
+            if(intentosFallidos == 8){
+                dibujarPiernaIz();
+            }
+
+            if(intentosFallidos == 9){
+                dibujarPiernaDerch();
+                mensajeDerrota();
+                dibujarGuiones();
+                ocultarBotones();
+                document.querySelector("#boton-jugar-nuevamente").style.display="block"
+            }   
+    }
+}
+
+function letrasErradas(){
+    tablero.font = "20px Arial";
+    tablero.fillStyle = "black";
+    tablero.fillText(letraEscrita, 325 + (20 * intentosFallidos) , 550);
+    intentosFallidos++;   
+}
+
+function verBoton(){
+    document.querySelector(".div-texto").style.display = "flex";
+}
+
+function ocultarBotones(){
+    document.querySelector(".div-texto").style.display = "none";
+}
+
+function agregarPalabra(){
+    let agregaPalabra = document.querySelector("#campo-agregar-palabra");
+    if(agregaPalabra.value == ""){
+        agregaPalabra.placeholder = "No se agregaron palabras";
+    }else{
+        palabras.push(agregaPalabra.value);
+        agregaPalabra.value = "";
+        agregaPalabra.placeholder = "Palabra agregada";
+        console.log(palabras)
+    }
+}
